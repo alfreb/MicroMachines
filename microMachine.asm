a473e3bd1dd960d7930a17736ee39927c439a866
Alfred Bratterud <alfred.bratterud@hioa.no>
2013-02-11 13:54:53 +0100

    Added serial-output from inside MMs

--- microMachine.asm
+++ microMachine.asm
@@ -1,6 +1,22 @@
-repeat:
-	hlt
-	jmp repeat
+boot:
+	mov     dx, 0	;Select COM1:
+        mov     al, '!'	;Character to transmit
+        mov     ah, 1	;Transmit opcode
+        int     14h
+        test    ah, 80h	;Check for error
+        jnz     SerialError
+
+
+	;; 	mov ah, 0Eh 		;0Eh -> Teletype output
+	;; 	mov al, '?'
+	;; 	int 10h		
+	
+run:				;Boot sequence done, do work
+	hlt			;Here, just idle
+	jmp run			;...forever
+
+SerialError:
+	
 	
 	times 510-($-$$) db 0	;
 	dw 0xAA55
